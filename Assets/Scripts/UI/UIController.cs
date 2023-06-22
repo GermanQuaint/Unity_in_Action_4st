91869c6b8162f4d72dc1855613f0d08696163ca2
German <40128615+GermanQuaint@users.noreply.github.com>
2023-06-22 17:48:46 +0300

    Initial commit

---
+++ Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] TMP_Text scoreLabel;
+    [SerializeField] SettingsPopup settingsPopup;
+
+    private int score;
+
+    void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+    void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void Start()
+    {
+        score = 0;
+        scoreLabel.text = score.ToString();
+
+        settingsPopup.Close();
+    }
+
+    private void OnEnemyHit()
+    {
+        score += 1;
+        scoreLabel.text = score.ToString();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnOpenSettings()
+    {
+        settingsPopup.Open();
+        Debug.Log("open settings");
+    }
+
+    public void OnPointerDown()
+    {
+        Debug.Log("pointer down");
+    }
+}
